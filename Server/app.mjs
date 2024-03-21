58061cc107af6aa844ae45f69738c332212ab773
Edondjiibolo <enzombul@gmail.com>
2024-03-21 14:51:43 +0800

    cors middleware

--- Server/app.mjs
+++ Server/app.mjs
@@ -2,12 +2,13 @@ import express from "express";
 import multer from "multer";
 import { corsMiddleware } from "./Middleware/cors.js";
 import { serviceRouter } from "./Routes/service-router.js";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 //Middleware para capturar el body de una reques en un post Y PARA EL CORS
-app.use(corsMiddleware());
+// app.use(corsMiddleware());
+app.use(cors());
 app.use(express.json());
 app.disable("x-powered-by");
 //TODO : CREAR UN MIDDLEWARE PARA LAS PETICIONES DEL USUARIO Y VERIFICAR SI ESTAN AUTENTICADOS Y AUTORIZADOS
